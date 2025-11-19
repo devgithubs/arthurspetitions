05e2d712a8423d6d75f3f620b172cb46b9591175
Arthur <arthuroleary.work@gmail.com>
2025-11-19 22:37:48 +0000

    Add dependencies to POM, set up packages

--- src/main/java/ie/nuigalway/ct5171/ArthursPetitionsApplication.java
+++ src/main/java/ie/nuigalway/ct5171/ArthursPetitionsApplication.java
@@ -1,8 +1,12 @@
 package ie.nuigalway.ct5171;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ArthursPetitionsApplication {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        SpringApplication.run(ArthursPetitionsApplication.class, args);
     }
 }
