1e3736a09df59309ded2e9aeeaf9569dcd97ec92
Arthur <arthuroleary.work@gmail.com>
2025-11-22 00:08:15 +0000

    update main class for WAR deployment

--- src/main/java/ie/nuigalway/ct5171/ArthursPetitionsApplication.java
+++ src/main/java/ie/nuigalway/ct5171/ArthursPetitionsApplication.java
@@ -2,9 +2,19 @@ package ie.nuigalway.ct5171;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ArthursPetitionsApplication {
+public class ArthursPetitionsApplication extends SpringBootServletInitializer {
+
+    // used when running as a WAR inside Tomcat
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ArthursPetitionsApplication.class);
+    }
+
+    // used when running from IntelliJ (fat jar)
     public static void main(String[] args) {
         SpringApplication.run(ArthursPetitionsApplication.class, args);
     }
